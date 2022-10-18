201090a7bf6b143faaec4b1f6bec31b4c78a2f37
BinaryByte <vishal01mehra@gmail.com>
2022-10-18 20:36:11 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,20 @@ const int trig = 5;
 #define SOUND_SPEED 0.034 //-cm/uS
 #define CM_TO_INCH 0.393701
 long Duration;
-float distanceCm;
-int tempDist;
+long Duration2;
 
-Servo  pitch, yaw; 
-int pos = 0;   
+float distanceCm;
+float tempDist;
+float deadband = 2;
+
+Servo pitch, yaw;
+int pos = 0;
 int pitchPin = 17;
 int YawPin = 16;
 int homepos;
 
-void home(){
+void home()
+{
   homepos = true;
   yaw.write(90);
   delay(150);
@@ -23,45 +27,70 @@ void home(){
   delay(150);
 }
 
-void setup() {
-  Serial.begin(115200);
-
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-	pitch.setPeriodHertz(50);
-  yaw.setPeriodHertz(50);
-	pitch.attach(pitchPin, 1000, 2000);
-  yaw.attach(YawPin, 1000, 2000);
-
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT); 
-  home();
-}
-
-void readDist(){
+void readDist()
+{
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
   Duration = pulseIn(echo, HIGH);
-  distanceCm = Duration * SOUND_SPEED/2;
+  distanceCm = Duration * SOUND_SPEED / 2;
+  delay(100);
 }
 
-void loop() {
-  readDist();
-  distanceCm = tempDist;
-  delay(500);
-  readDist();
-  if(distanceCm != tempDist){
-    int rndm = random(50,120);
+void readDist2()
+{
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  Duration2 = pulseIn(echo, HIGH);
+  tempDist = Duration2 * SOUND_SPEED / 2;
+  delay(100);
+}
+
+void moveRandom(){
+    int rndm = random(50, 120);
     int rndm2 = random(50, 120);
     pitch.write(rndm);
     yaw.write(rndm2);
     delay(500);
-  }
-
 }
 
+
+void setup()
+{
+  Serial.begin(9600);
+
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  pitch.setPeriodHertz(50);
+  yaw.setPeriodHertz(50);
+  pitch.attach(pitchPin, 1000, 2000);
+  yaw.attach(YawPin, 1000, 2000);
+
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  home();
+  readDist2();
+}
+
+
+
+void loop()
+{
+  readDist();
+  if (tempDist - distanceCm > deadband)
+  {
+   moveRandom();
+   readDist();
+   delay(500);
+  }
+  readDist2();
+  Serial.print("Difference: ");
+  Serial.println(tempDist - distanceCm);
+}
